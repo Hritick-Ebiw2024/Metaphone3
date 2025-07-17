3617dc0ae09f6d91dbcbf0dca801ca9653caeec0
Hritick-Ebiw2024 <165788499+Hritick-Ebiw2024@users.noreply.github.com>
2025-07-17 11:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -323,7 +323,7 @@ if __name__ == '__main__':
     # Start the Flask development server
     app.run(
         host='0.0.0.0',
-        port=5000,
+        port=8080,
         debug=True,
         threaded=True
     )
